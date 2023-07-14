145e42a1720d8f9726827c748770e08f7a5f172c
svakhil00 <svakhil00@gmail.com>
2023-07-14 13:00:33 -0700

    better ui for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,68 @@
-""
-import Link from 'next/link'
-import Image from 'next/image'
-import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
-import { getPokemon } from './helpers'
-import { getPicture } from './helpers'
-import card from '@/components/Card'
+import Image from 'next/image';
+import { getPokemon } from './helpers';
+import { getPicture } from './helpers';
+import { Label } from '@/components/ui/label';
+import { ScrollArea } from '../../@/components/ui/scroll-area';
+import {
+  Table,
+  TableCell,
+} from "@/components/ui/table"
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+
+
 
 export default async function Home() {
 
-  const pokemonList = new Array();
-  for (let i = 1; i < 10; i++) {
+  const pokemonList = []
+  for (let i = 1; i < 26; i++) {
     pokemonList[i] = (await getPokemon(i));
   }
 
 
   const pokemon = pokemonList.map((info) => {
-    const nameNumber = info.Item.name.S.charAt(0).toUpperCase() + info.Item.name.S.slice(1) + "#" + info.Item.id.N.padStart(4, '0')
+    const name = info.Item.name.S.charAt(0).toUpperCase() + info.Item.name.S.slice(1)
+    const number = "#" + info.Item.id.N.padStart(4, '0')
     return (
-      <a href={`/${info.Item.id.N}`} key={info.Item.id.N}>
-        <td >{card(info, nameNumber)}</td>
-      </a>
+      <div className="border-spacing-10" key={info.Item.id.N}>
+        <a href={`/${info.Item.id.N}`}>
+          {makeCard(info, name, number)}
+        </a>
+      </div>
     )
   });
 
   return (
-    <div>
-      <h1>Pokedex</h1>
-      <table>{pokemon}</table>
+    <div className="h-screen">
+      <h1 className="text-6xl font-bold text-center bg-red-600 text-white h-[10%]">Pokedex</h1>
+      <div className='bg-slate-950 h-[90%]'>
+        <ScrollArea className="rounded-md border p-8 h-full">
+          <div className="flex flex-wrap bg-slate-950 gap-10 justify-center">
+            {pokemon}
+          </div>
+        </ScrollArea>
+      </div>
+
     </div>
+  )
+}
+
+function makeCard(info, name, number) {
+  return (
+    <Card className="w-[250px] bg-slate-200">
+      <CardContent>
+        <div className="grid w-full items-center gap-4">
+          <div>
+            <Image src={getPicture(info)} width={250} height={250} alt="Image Not Found"></Image>
+          </div>
+          <div>
+            <Label htmlFor="name">{name}<br></br>{number}</Label>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+
   )
 }
